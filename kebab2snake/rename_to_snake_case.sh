2b2e967860dd4faa168bda7ba075c081b997d9ab
mendezm <mendezm@rose-hulman.edu>
2025-07-01 22:26:31 -0600

    Added kebab2snake tool

---
+++ kebab2snake/rename_to_snake_case.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+source "${COLORS}" 
+
+_do_rename() {
+	local file="$1"
+	local is_silent="$2"
+	local path=$( dirname "${file}" )
+	local current_name=$( basename "${file}" )
+	local new_name="${current_name//-/_}"
+
+	if [[ "${current_name}" != "${new_name}" ]]; then
+		if [[ "${is_silent}" == true ]]; then
+			mv "${file}" "${path}/${new_name}"
+		else
+			mv -v "${file}" "${path}/${new_name}"
+		fi
+	else
+		if [[ "${is_silent}" != true ]]; then
+			echo -e "${ORANGE}No change needed: ${WHITE}${current_name} is already snake_case."
+		fi
+	fi
+
+	echo "${path}/${new_name}"
+}
+
+_rename_recursive() {
+	local current_name="$1"
+	local is_silent="$2"
+	local max_depth="$3"
+	local current_depth="$4"
+
+	local new_name=$( _do_rename "${current_name}" "${is_silent}" )
+
+	shopt -s nullglob
+	for entry in "${new_name}"/*; do
+		local entry_depth=$(( current_depth + 1 ))
+
+		if [[ -n "${max_depth}" && "${entry_depth}" -gt "${max_depth}" ]]; then
+			continue
+		elif [[ -f "${entry}" ]]; then
+			_do_rename "${entry}" "${is_silent}"
+		elif [[ -d "${entry}" ]]; then
+			_rename_recursive "${entry}" "${is_silent}" "${max_depth}" "${entry_depth}"
+		else
+			continue
+		fi
+	done
+	shopt -u nullglob
+}
+
+rename_to_snake_case() {
+	local is_silent=false
+	local max_depth=""
+	local file=""
+
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			--silent)
+				is_silent=true
+				shift
+				;;
+			--max-depth)
+				max_depth="$2"
+				if ! [[ "${max_depth}" =~ ^[0-9]+$ ]]; then
+					echo "Error: --max-depth must be a non-negative integer." >&2
+					return 1
+				fi
+
+				shift 2
+				;;
+			*)
+				file="$1"
+				shift
+				;;
+		esac
+	done
+
+	if [[ -z "${file}" || ! -e "${file}" ]]; then
+		echo "Usage: $0 [--silent] [--max-depth N] <file-name>|<directory-name>"
+		return 1
+	elif [[ -f "${file}" ]]; then
+		_do_rename "${file}" "${is_silent}"
+	elif [[ -d "${file}" ]]; then
+		_rename_recursive "${file}" "${is_silent}" "${max_depth}" 0
+	fi
+
+	return 0
+}
+
---
+++ kebab2snake/test.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+
+source "${COLORS}"
+source "./rename_to_snake_case.sh"
+
+test_rename_to_snake_case_on_kebab_case_file() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	touch kebab-case-file.txt
+
+	rename_to_snake_case --silent kebab-case-file.txt > /dev/null 2>&1
+
+	if [[ -f "kebab_case_file.txt" ]]; then
+		echo -e "${GREEN}Test passed: ${WHITE}File in kebab case was renamed successfully."
+	else
+		echo -e "${RED}Test failed: ${WHITE}File in kebab case was not renamed successfully."
+	fi
+
+	cd ..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_kebab_case_directory() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	mkdir kebab-case-directory
+
+	rename_to_snake_case --silent kebab-case-directory > /dev/null 2>&1
+
+	if [[ -d "kebab_case_directory" ]]; then
+		echo -e "${GREEN}Test passed: ${WHITE}Directory in kebab case was renamed successfully."
+	else
+		echo -e "${RED}Test failed: ${WHITE}Directory in kebab case was not renamed successfully."
+	fi
+
+	cd ..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_snake_case_file() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	touch snake_case_file.txt
+
+	rename_to_snake_case --silent snake_case_file.txt > /dev/null 2>&1
+
+	if [[ -f "snake_case_file.txt" ]]; then
+		echo -e "${GREEN}Test passed: ${WHITE}File in snake case was not renamed."
+	else
+		echo -e "${RED}Test failed: ${WHITE}File in snake case was renamed."
+	fi
+
+	cd ..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_snake_case_directory() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	mkdir snake_case_directory
+
+	rename_to_snake_case --silent snake_case_directory > /dev/null 2>&1
+
+	if [[ -d "snake_case_directory" ]]; then
+		echo -e "${GREEN}Test passed: ${WHITE}Directory in snake case was not renamed."
+	else
+		echo -e "${RED}Test failed: ${WHITE}Directory in snake was renamed."
+	fi
+
+	cd ..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_kebab_case_directory_with_one_file_and_one_empty_directory() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	mkdir top-level-kebab-case-directory
+	touch top-level-kebab-case-directory/kebab-case-file.txt
+	mkdir top-level-kebab-case-directory/empty-kebab-case-directory
+
+	rename_to_snake_case --silent top-level-kebab-case-directory > /dev/null 2>&1
+
+	echo -e "${WHITE}Test: ${CYAN}Kebab case directory with one file and one empty directory${WHITE}"
+
+	local is_failed=false
+	if [[ -d "top_level_kebab_case_directory" ]]; then
+		cd top_level_kebab_case_directory
+		echo -e "${YELLOW}Level 0: ${WHITE}Top level directory was successfully renamed."
+	else
+		is_failed=true
+		cd top-level-kebab-case-directory
+		echo -e "${BLUE}Level 0: ${WHITE}Top level directory was not successfully renamed."
+	fi
+
+	if [[ -f "kebab_case_file.txt" ]]; then
+		echo -e "${YELLOW}Level 1: ${WHITE}Kebab case file was successfully renamed."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 1: ${WHITE} Kebab case file was not successfully renamed."
+	fi
+
+	if [[ -d "empty_kebab_case_directory" ]]; then
+		echo -e "${YELLOW}Level 1: ${WHITE}Empty kebab case directory was successfully renamed."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 1: ${WHITE}Empty kebab case directory was not successfully renamed."
+	fi
+
+	if [[ ${is_failed} == true ]]; then
+		echo -e "${RED}Test failed.${WHITE}"
+	else
+		echo -e "${GREEN}Test passed.${WHITE}"
+	fi
+
+	cd ../..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_with_negative_max_depth() {
+	mkdir test_environment
+	
+	rename_to_snake_case --max-depth -1 test_environment > /dev/null 2>&1
+
+	if [[ -d "test_environment" ]]; then
+		echo -e "${GREEN}Test passed: ${WHITE}test_environment was not modified when max-depth was negative."
+	else
+		echo -e "${RED}Test failed: ${WHITE}test_environment was modified when max-depth was negative."
+	fi
+
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_kebab_case_directory_with_two_files_and_one_empty_directory_and_one_populated_directory() {
+	mkdir test_environment
+	cd test_environment || { echo "Unable to enter test_environment"; return 1; }
+	mkdir top-level-kebab-case-directory
+	touch top-level-kebab-case-directory/kebab-case-file.txt
+	mkdir top-level-kebab-case-directory/empty-kebab-case-directory
+	mkdir top-level-kebab-case-directory/populated-kebab-case-directory
+	touch top-level-kebab-case-directory/populated-kebab-case-directory/another-kebab-case-file.txt
+	touch top-level-kebab-case-directory/populated-kebab-case-directory/final-kebab-case-file-to-be-touched.txt
+	mkdir top-level-kebab-case-directory/populated-kebab-case-directory/final-kebab-case-directory-to-be-touched
+	touch top-level-kebab-case-directory/populated-kebab-case-directory/final-kebab-case-directory-to-be-touched/should-remain-kebab-case-file.txt
+	mkdir top-level-kebab-case-directory/populated-kebab-case-directory/final-kebab-case-directory-to-be-touched/should-remain-kebab-case-directory
+	touch top-level-kebab-case-directory/populated-kebab-case-directory/final-kebab-case-directory-to-be-touched/should-remain-kebab-case-directory/another-untouched-kebab-case-file.txt
+
+	rename_to_snake_case --silent --max-depth 2 top-level-kebab-case-directory > /dev/null 2>&1
+
+	echo -e "${WHITE}Test: ${CYAN}Kebab directory with nested files and directories (max-depth=2)${WHITE}"
+
+	local is_failed=false
+	if [[ -d "top_level_kebab_case_directory" ]]; then
+		cd top_level_kebab_case_directory
+		echo -e "${YELLOW}Level 0: ${WHITE}Top level directory was successfully named."
+	else
+		is_failed=true
+		cd top-level-kebab-case-directory
+		echo -e "${BLUE}Level 0: ${WHITE}Top level directory was not successfully named."
+	fi
+
+	if [[ -f "kebab_case_file.txt" ]]; then
+		echo -e "${YELLOW}Level 1: ${WHITE}First file was successfully named."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 1: ${WHITE}First file was not successfully named."
+	fi
+
+	if [[ -d "empty_kebab_case_directory" ]]; then
+		echo -e "${YELLOW}Level 1: ${WHITE}Empty directory was successfully named."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 1: ${WHITE}Empty directory was not successfully named."
+	fi
+
+	if [[ -d "populated_kebab_case_directory" ]]; then
+		cd populated_kebab_case_directory
+		echo -e "${YELLOW}Level 1: ${WHITE}Populated directory was successfully named."
+	else
+		is_failed=true
+		cd populated-kebab-case-directory
+		echo -e "${BLUE}Level 1: ${WHITE}Populated directory was not successfully named."
+	fi
+
+	if [[ -f "another_kebab_case_file.txt" ]]; then
+		echo -e "${YELLOW}Level 2: ${WHITE}File was sucessfully named."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 2: ${WHITE}File was not successfully named."
+	fi
+
+	if [[ -d "final_kebab_case_directory_to_be_touched" ]]; then
+		cd final_kebab_case_directory_to_be_touched
+		echo -e "${YELLOW}Level 2: ${WHITE}Directory was successfully named."
+	else
+		is_failed=true
+		cd final-kebab-case-directory-to-be-touched
+		echo -e "${BLUE}Level 2: ${WHITE}Directory was not successfully named."
+	fi
+
+	if [[ -f "should-remain-kebab-case-file.txt" ]]; then
+		echo -e "${YELLOW}Level 3: ${WHITE}File kept original name."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 3: ${WHITE}File did not keep original name."
+	fi
+
+	if [[ -d "should-remain-kebab-case-directory" ]]; then
+		cd should-remain-kebab-case-directory
+		echo -e "${YELLOW}Level 3: ${WHITE}Directory kept original name."
+	else
+		is_failed=true
+		cd should_remain_kebab_case_directory
+		echo -e "${YELLOW}Level 3: ${WHITE}Directory did not keep original name."
+	fi
+
+	if [[ -f "another-untouched-kebab-case-file.txt" ]]; then
+		echo -e "${YELLOW}Level 4: ${WHITE}File kept original name."
+	else
+		is_failed=true
+		echo -e "${BLUE}Level 4: ${WHITE}File did not keep original name."
+	fi
+
+	if [[ ${is_failed} == true ]]; then
+		echo -e "${RED}Test failed.${WHITE}"
+	else
+		echo -e "${GREEN}Test passed.${WHITE}"
+	fi
+
+	cd ../../../../..
+	rm -r test_environment
+}
+
+test_rename_to_snake_case_on_kebab_case_file
+test_rename_to_snake_case_on_kebab_case_directory
+test_rename_to_snake_case_on_snake_case_file
+test_rename_to_snake_case_on_snake_case_directory
+test_rename_to_snake_case_on_kebab_case_directory_with_one_file_and_one_empty_directory
+test_rename_to_snake_case_with_negative_max_depth
+test_rename_to_snake_case_on_kebab_case_directory_with_two_files_and_one_empty_directory_and_one_populated_directory
+
